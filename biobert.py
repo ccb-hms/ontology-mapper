0af5cfb51b66f93cad612e32e0eb356920dbd3fc
krico1 <krico@hmc.edu>
2021-11-05 02:10:19 -0700

    added more to biobert.py

--- biobert.py
+++ biobert.py
@@ -5,7 +5,12 @@ def biobert_mapper(input):
     list_of_lines = []
     for line in input:
         list_of_lines.append(nlu.load('biobert').predict(line, output_level='sentence'))
-    print(list_of_lines)
+    textfile = open('output.txt', "w")
+    for element in list_of_lines:
+        textfile.write(element + "\n")
+    textfile.close()
+    return list_of_lines
+
 
 # def cal_cosine(vec1, vec2):
 #     return inner(vec1, vec2)/sqrt(np.dot(vec1, vec1)*dot(vec2, vec2))
