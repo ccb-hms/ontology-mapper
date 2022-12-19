030adae8d3a928eecbb6e47338d7dc7ca6224fb4
Jason Payne <paynejason@gmail.com>
2022-12-19 11:44:57 -0500

    Bug Fixes in term_collector.py
    Fixes the known bugs in the filtering function, which prevented the user from filtering on the base IRIs while using a cached ontology. Also fixed a bug that prevented the tool from working with stored terms.

--- text2term/__init__.py
+++ text2term/__init__.py
@@ -1,3 +1,7 @@
 from .t2t import map_terms
 from .t2t import map_file
+from .t2t import cache_ontology_set
+from .t2t import cache_ontology
+from .t2t import cache_exists
+from .t2t import clear_cache
 from .mapper import Mapper
